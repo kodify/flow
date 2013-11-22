56f2f04467dbca882867e04ff1460fdc99ca9cd9
Adria Cidre <adria.cidre@gmail.com>
2013-11-22 09:34:11 +0100

    supu

--- lib/config.rb
+++ lib/config.rb
@@ -7,7 +7,7 @@ module Flow
 
     def self.get
       @__config__ ||= begin
-        return ENV['FLOW_CONFIG'] if ENV.include? 'FLOW_CONFIG'
+        # return eval(ENV['FLOW_CONFIG']) if ENV.include? 'FLOW_CONFIG'
         YAML::load_file(File.join(File.dirname(__FILE__), '../config/parameters.yml'))['parameters']
       end
     end
